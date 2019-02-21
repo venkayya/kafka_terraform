c57e3a08dcb6b27c6f968d3096792f0bd0dbe3de
venkayyanaidu@google.com <venkayyanaidu@gflocks.com>
2019-02-21 14:40:33 -0800

    check

--- certs-create.sh
+++ certs-create.sh
@@ -5,115 +5,70 @@
 #    -o verbose \
 #    -o xtrace
 
+# Cleanup files
+rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12
+
 # Generate CA key
-				openssl req \
-        				-new \
-        				-x509 \
-        				-keyout snakeoil-ca-1.key \
-								-out snakeoil-ca-1.crt \
-								-days 365 \
-								-subj '/CN=ca1.test.serversecrect.io/OU=TEST/O=serversecrect/L=PaloAlto/S=Ca/C=US' \
-								-passin pass:serversecrect \
-								-passout pass:serversecrect
+openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
-# Create host keystore
-				keytool -genkey \
-								-noprompt \
-								-alias $(hostname) \
-								-dname "CN=$(hostname),OU=TEST,O=serversecrect,L=PaloAlto,S=Ca,C=US" \
-								-ext "SAN=dns:$(hostname),dns:localhost" \
-								-keystore kafka.$(hostname).keystore.jks \
-								-keyalg RSA \
-								-storepass serversecrect \
-								-keypass serversecrect
+for i in kafka1 kafka2 kafka3
+do
+	echo "------------------------------- $i -------------------------------"
 
-# Create the certificate signing request (CSR)
-				keytool -keystore kafka.$(hostname).keystore.jks \
-								-alias $(hostname) \
-								-certreq -file $(hostname).csr \
-								-storepass serversecrect \
-								-keypass serversecrect \
-								-ext "SAN=dns:$(hostname),dns:localhost"
+	# Create host keystore
+	keytool -genkey -noprompt \
+				 -alias $i \
+				 -dname "CN=$i,OU=TEST,O=CONFLUENT,L=PaloAlto,S=Ca,C=US" \
+                                 -ext "SAN=dns:$i,dns:localhost" \
+				 -keystore kafka.$i.keystore.jks \
+				 -keyalg RSA \
+				 -storepass confluent \
+				 -keypass confluent
 
-# Sign the host certificate with the certificate authority (CA)
-				openssl x509 \
-								-req \
-								-CA snakeoil-ca-1.crt \
-								-CAkey snakeoil-ca-1.key \
-								-in $(hostname).csr \
-								-out $(hostname)-ca1-signed.crt \
-								-days 9999 \
-								-CAcreateserial \
-								-passin pass:serversecrect \
-								-extensions v3_req \
-								-extfile <(cat <<EOF
+	# Create the certificate signing request (CSR)
+	keytool -keystore kafka.$i.keystore.jks -alias $i -certreq -file $i.csr -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
+        #openssl req -in $i.csr -text -noout
+
+        # Sign the host certificate with the certificate authority (CA)
+        openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in $i.csr -out $i-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent -extensions v3_req -extfile <(cat <<EOF
 [req]
 distinguished_name = req_distinguished_name
 x509_extensions = v3_req
 prompt = no
 [req_distinguished_name]
-CN = $(hostname)
+CN = $i
 [v3_req]
 subjectAltName = @alt_names
 [alt_names]
-DNS.1 = $(hostname)
+DNS.1 = $i
 DNS.2 = localhost
 EOF
 )
+        #openssl x509 -noout -text -in $i-ca1-signed.crt
 
-# Sign and import the CA cert into the keystore
-				keytool -noprompt \
-								-keystore kafka.$(hostname).keystore.jks \
-								-alias CARoot \
-								-import -file snakeoil-ca-1.crt \
-								-storepass serversecrect \
-								-keypass serversecrect
+        # Sign and import the CA cert into the keystore
+	keytool -noprompt -keystore kafka.$i.keystore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass confluent -keypass confluent
+        #keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent
 
-# Sign and import the host certificate into the keystore
-				keytool -noprompt \
-	        			-keystore kafka.$(hostname).keystore.jks \
-								-alias $(hostname) \
-								-import -file $(hostname)-ca1-signed.crt \
-								-storepass serversecrect \
-								-keypass serversecrect \
-								-ext "SAN=dns:$(hostname),dns:localhost"
+        # Sign and import the host certificate into the keystore
+	keytool -noprompt -keystore kafka.$i.keystore.jks -alias $i -import -file $i-ca1-signed.crt -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
+        #keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent
 
-# Create truststore and import the CA cert
-				keytool -noprompt \
-	        			-keystore kafka.$(hostname).truststore.jks \
-								-alias CARoot \
-								-import -file snakeoil-ca-1.crt \
-								-storepass serversecrect \
-								-keypass serversecrect
+	# Create truststore and import the CA cert
+	keytool -noprompt -keystore kafka.$i.truststore.jks -alias CARoot -import -file snakeoil-ca-1.crt -storepass confluent -keypass confluent
 
-# Save creds
-#  			echo "serversecrect" > ${i}_sslkey_creds
-#  			echo "serversecrect" > ${i}_keystore_creds
-#  			echo "serversecrect" > ${i}_truststore_creds
+	# Save creds
+  	echo "confluent" > ${i}_sslkey_creds
+  	echo "confluent" > ${i}_keystore_creds
+  	echo "confluent" > ${i}_truststore_creds
 
-# Create pem files and keys used for Schema Registry HTTPS testing
-				keytool -export \
-	        			-alias $(hostname) \
-								-file $(hostname).der \
-								-keystore kafka.$(hostname).keystore.jks \
-								-storepass serversecrect
+	# Create pem files and keys used for Schema Registry HTTPS testing
+	#   openssl x509 -noout -modulus -in client.certificate.pem | openssl md5
+	#   openssl rsa -noout -modulus -in client.key | openssl md5
+    #   echo "GET /" | openssl s_client -connect localhost:8085/subjects -cert client.certificate.pem -key client.key -tls1
+	keytool -export -alias $i -file $i.der -keystore kafka.$i.keystore.jks -storepass confluent
+	openssl x509 -inform der -in $i.der -out $i.certificate.pem
+	keytool -importkeystore -srckeystore kafka.$i.keystore.jks -destkeystore $i.keystore.p12 -deststoretype PKCS12 -deststorepass confluent -srcstorepass confluent -noprompt
+	openssl pkcs12 -in $i.keystore.p12 -nodes -nocerts -out $i.key -passin pass:confluent
 
-				openssl x509 \
-	        			-inform der \
-								-in $(hostname).der \
-								-out $(hostname).certificate.pem
-
-				keytool -importkeystore \
-	        			-srckeystore kafka.$(hostname).keystore.jks \
-								-destkeystore $(hostname).keystore.p12 \
-								-deststoretype PKCS12 \
-								-deststorepass serversecrect \
-								-srcstorepass serversecrect \
-								-noprompt
-
-				openssl pkcs12 \
-	        			-in $(hostname).keystore.p12 \
-	        			-nodes \
-	        			-nocerts \
-	        			-out $(hostname).key \
-	        			-passin pass:serversecrect
+done
